b1b9834e78f05dbf0abeb60c97b3f7e86ee8c91c
malwandemoyo <malwande129@gmail.com>
2024-01-29 22:11:49 +0200

    Made the whole card section clickable

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -3,12 +3,15 @@
 </script>
 
 
-<div class=" m-6 border border-black max-w-xs rounded shadow-lg transition-transform transform hover:scale-105 flex flex-col items-center">
+<div class="m-6 border border-black max-w-xs rounded shadow-lg transition-transform transform hover:scale-105 flex flex-col items-center">
     <div class="font-bold p-4 flex flex-col justify-center items-center">
       
         <div class="flex flex-col justify-center items-center">
-            <div class="flex justify-center">
-                <img src="/january27.webp" class="rounded-lg " alt="" srcset="">
+            <div >
+                <a href="/events/january27" class="flex justify-center">
+                    <img src="/january27.webp" class="rounded-lg " alt="" srcset="">
+                </a>
+                
             </div>
             <div class="mt-4 font-sans text-gray-700 text-xl">
                 <h1><a href="/events/january27">January 2024 Meetup</a></h1>
