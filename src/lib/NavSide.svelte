d8899378e0936350e08a567de36830314b66a059
malwandemoyo <n02221161t@students.nust.ac.zw>
2024-02-12 15:02:48 +0200

    Added Home, Blog, Events, and Gallery to nav bar

--- src/lib/NavSide.svelte
+++ src/lib/NavSide.svelte
@@ -5,7 +5,10 @@
 <main>
     <div class="hidden md:flex flex-row">
         <ul class="flex flex-row space-x-8 items-center">
-            <li><a href="/events" class="hover:underline text-2xl">EVENTS</a></li>	
+            <li><a href="/" class="hover:underline text-2xl">HOME</a></li>
+            <li><a href="/events" class="hover:underline text-2xl">BLOG</a></li>
+            <li><a href="/events" class="hover:underline text-2xl">EVENTS</a></li>
+            <li><a href="/events" class="hover:underline text-2xl">GALLERY</a></li>	
             <li><a href="/projects" class="hover:underline text-2xl">PROJECTS</a></li>
             <div class="scale-x-[-1]">
                 <LightSwitch />
