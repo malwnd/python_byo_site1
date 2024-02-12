f7faa31816be64db20bcaf81f4cb74f9c527ece6
malwandemoyo <n02221161t@students.nust.ac.zw>
2024-02-12 15:05:27 +0200

    Added Elements to nav bar

--- src/lib/MobileNav.svelte
+++ src/lib/MobileNav.svelte
@@ -5,7 +5,11 @@
 <main>
         <nav class="list-nav md:hidden p-4 flex justify-center">
             <ul>
+                <li class=""><a href="/blog" class="justify-center hover:underline text-2xl">BLOG</a></li>
+                <li class=""><a href="/" class="justify-center hover:underline text-2xl">HOME</a></li>	
                 <li class=""><a href="/events" class="justify-center hover:underline text-2xl">EVENTS</a></li>	
+                <li class=""><a href="/gallery" class="justify-center hover:underline text-2xl">GALLERY</a></li>	
+
                 <li><a href="/projects" class="hover:underline text-2xl">PROJECTS</a></li>
                 <div class="p-4 m-4 flex justify-center scale-x-[-1]">
                     <LightSwitch />
--- src/lib/NavSide.svelte
+++ src/lib/NavSide.svelte
@@ -6,9 +6,9 @@
     <div class="hidden md:flex flex-row">
         <ul class="flex flex-row space-x-8 items-center">
             <li><a href="/" class="hover:underline text-2xl">HOME</a></li>
-            <li><a href="/events" class="hover:underline text-2xl">BLOG</a></li>
+            <li><a href="/blog" class="hover:underline text-2xl">BLOG</a></li>
             <li><a href="/events" class="hover:underline text-2xl">EVENTS</a></li>
-            <li><a href="/events" class="hover:underline text-2xl">GALLERY</a></li>	
+            <li><a href="/gallery" class="hover:underline text-2xl">GALLERY</a></li>	
             <li><a href="/projects" class="hover:underline text-2xl">PROJECTS</a></li>
             <div class="scale-x-[-1]">
                 <LightSwitch />
