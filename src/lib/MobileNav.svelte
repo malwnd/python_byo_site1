116ebd62d9e23c3c052009c8b95718961fa8f554
malwandemoyo <n02221161t@students.nust.ac.zw>
2024-02-12 15:09:02 +0200

    Blog after hoem

--- src/lib/MobileNav.svelte
+++ src/lib/MobileNav.svelte
@@ -5,8 +5,8 @@
 <main>
         <nav class="list-nav md:hidden p-4 flex justify-center">
             <ul>
-                <li class=""><a href="/blog" class="justify-center hover:underline text-2xl">BLOG</a></li>
-                <li class=""><a href="/" class="justify-center hover:underline text-2xl">HOME</a></li>	
+                <li class=""><a href="/" class="justify-center hover:underline text-2xl">HOME</a></li>
+                <li class=""><a href="/blog" class="justify-center hover:underline text-2xl">BLOG</a></li> 	
                 <li class=""><a href="/events" class="justify-center hover:underline text-2xl">EVENTS</a></li>	
                 <li class=""><a href="/gallery" class="justify-center hover:underline text-2xl">GALLERY</a></li>	
 
